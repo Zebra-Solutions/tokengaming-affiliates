4a039b1efafe8b48130778a7528e1c9a663ff5ba
MakiLe <marijalekic92@gmail.com>
2024-11-04 02:14:00 +0100

    Fix: slide in effect

--- app/components/Join.tsx
+++ app/components/Join.tsx
@@ -7,11 +7,11 @@ const Join: React.FC = () => {
       className="py-0 bg-[#161617] text-gray-50 flex flex-col md:flex-row items-center justify-center" // Change 'justify-around' to 'justify-center'
     >
       <div className="w-full md:w-1/2 text-right md:text-right px-8 mb-8 md:mb-0">
-        <p className="text-gray-200 text-5xl">
+        <p className="text-gray-200 text-5xl slide-in">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
           perferendis?
         </p>
-        <button className="px-6 py-3 mt-4 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-800 transition">
+        <button className="px-6 py-3 mt-4 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-800 transition slide-in-pulse">
           JOIN NOW
         </button>
       </div>
