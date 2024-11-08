8c2d271607dec67b120929153c3619e7f9866c4d
MakiLe <marijalekic92@gmail.com>
2024-11-08 20:36:11 +0100

    Fix: Symbol rotation and mobile view

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -28,7 +28,7 @@ const Testimonials: React.FC = () => {
 
   return (
     <section id="testimonials" className="py-20 px-4 bg-[#161617] text-gray-50">
-      <h2 className="text-3xl font-bold text-center mb-8">Testimonials</h2>
+      <h2 className="text-3xl font-bold text-center mb-24">Testimonials</h2>
 
       <div className="flex justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-20 mx-auto w-9/12">
