99c47f84902c1d6dbc8e9bd0c76794efb896db05
Naja001 <158475945+Naja001@users.noreply.github.com>
2024-12-03 17:57:20 +0100

    bg in layout

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,13 +1,13 @@
 
-import logBG from '@/public/Cardblue1.jpg'
+import logBG from '@/public/bg-home.png'
 type Props = {
   children?: React.ReactNode;
 };
 export default function LoggedOutLayout({ children }: Props) {
   return (
     <>
-      <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24 bg-[#161617] bg-cover bg-center" style={{backgroundImage:`url(${logBG.src})`, backgroundBlendMode: "overlay",
-        backgroundColor: "rgba(0, 0, 0, 0.5)"}}>
+      <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24  bg-cover bg-center" style={{backgroundImage:`url(${logBG.src})`,
+        backgroundColor: "#161617"}}>
         {children}
       </div>
       
