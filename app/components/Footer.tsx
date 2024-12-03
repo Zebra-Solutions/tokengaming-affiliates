7dbaa84b6fbe2c1e8ce18bb20e62b1b66da12727
MakiLe <marijalekic92@gmail.com>
2024-12-03 13:26:38 +0100

    Fix: color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full bg-[#000] text-gray-50 rounded-lg shadow">
-      <hr className=" border-gray-200 dark:border-gray-700 w-full" />
+    <footer className="w-full bg-[#161617] text-gray-50 rounded-lg shadow">
+      <hr className=" border-[#f2f3f5] dark:border-gray-700 w-full" />
 
       <div className="max-w-screen-xl mx-auto p-4 md:py-8">
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -62,7 +62,7 @@ export default function Header({ className = "" }: { className?: string }) {
     <>
       <header
         className={clsx(
-          "fixed top-0 w-full flex justify-between items-center p-4 z-50 lg:px-20 overflow-hidden",
+          "fixed top-0 w-full flex justify-between items-center z-50 lg:px-36 overflow-hidden px-36 py-4",
           className
         )}
         style={{
@@ -77,7 +77,7 @@ export default function Header({ className = "" }: { className?: string }) {
           </NavLink>
 
           {/* Navigation Links (Visible on large screens and above) */}
-          <nav className="hidden lg:flex lg:space-x-9 ml-24">
+          <nav className="hidden lg:flex lg:space-x-9 ml-16">
             {sections.map(({ id, label }) => (
               <NavLink
                 key={id} // Stable key based on section ID
