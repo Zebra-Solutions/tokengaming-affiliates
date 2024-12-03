dbce2728cd3882812a69992df9ade121ef490f75
MakiLe <marijalekic92@gmail.com>
2024-12-03 16:33:43 +0100

    Fix: Banner -picture

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -8,10 +8,10 @@ const Banner: React.FC = () => {
   return (
     <section
       style={{ height: `calc(100vh - 250px)` }}
-      className="  text-gray-50 flex flex-col-reverse md:flex-row items-center justify-between px-6 lg:px-20 main-font"
+      className="  text-gray-50 flex flex-col-reverse md:flex-row items-center justify-between px-6 lg:px-36 main-font relative"
     >
       {/* Text Section */}
-      <div className="w-full md:w-1/2 text-left md:text-right md:mb-0 space-y-6">
+      <div className="w-full md:w-1/2 text-left md:text-right md:mb-0 space-y-6 z-20">
         <h1 className="text-4xl md:text-5xl font-bold tracking-tight leading-tight">
           {["Promote.", " ", "Share. ", "Earn. "].map((word, index) => (
             <span
@@ -31,11 +31,13 @@ const Banner: React.FC = () => {
         </p>
       </div>
 
-      {/* Folder Card */}
-      <div className="w-full md:w-1/2 flex justify-center">
-        <div className="folder-slide-animation">
-          <FolderCard />
-        </div>
+      {/* Image Section */}
+      <div className="absolute w-2/3 h-5/6 right-0 top-0 -ml-20 rounded-l-full z-10 bg-blend-overlay bg-[#96969680]">
+        <img
+          src="./monkey.png" // Replace with your image
+          alt="Banner"
+          className="object-cover w-full h-full rounded-l-full"
+        />
       </div>
     </section>
   );
